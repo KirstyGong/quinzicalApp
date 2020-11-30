9d75b6aa2445417cd0816216404b294be324b834
Kirsty Gong <kirstygong23@gmail.com>
2020-11-30 22:34:54 +1300

    Created game page

---
+++ src/Scenes/GamePage/GamePage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class gamePage extends Component{
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default gamePage
